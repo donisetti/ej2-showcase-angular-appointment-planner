60469d50e6317a8d93f75712aa2233f4977ca289
Donisetti Ferreira Cosma <donisetti@msn.com>
2023-08-26 17:27:30 -0400

    Tradução do sistema

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+
+// Registering Syncfusion license key
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NGaF1cXGNCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWXdcdnVQQ2BYWEx2WEo=');
 
 if (environment.production) {
   enableProdMode();
